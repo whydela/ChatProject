d58869560821b05ad6ba75437b61d0c38c36d4c4
whydela <t.delato@studenti.unipi.it>
2022-10-12 11:44:30 +0200

    first_try

--- Server.c
+++ Server.c
@@ -8,8 +8,52 @@
 #include <unistd.h>
 #include <time.h>
 
+void first_print(){
+    int i;
+    printf("\n");
+    for(i=0; i < 190; i++){
+        printf("*");
+    }
+    printf("\n");
+    for (i=0; i < 11; i++){
+        printf("\t");
+    }
+    printf("SERVER AVVIATO\n");
+    for(i=0; i < 190; i++){
+        printf("*");
+    }
+    printf("\n\n\nInserisci un comando:\n");
+    printf("\n- list: Mostra gli utenti online.\n");
+    printf("- esc:  Chiude il server.\n");
+    printf("\n(Digitare help per i dettagli dei comandi)\n\n\n\n");
+    
 
+}
 
+int listen_config(struct sockaddr_in* addr, int port){
+
+    int sd;
+    int ret;
+    
+    sd = socket(AF_INET, SOCK_STREAM, 0);   // Creazione socket
+    memset(addr, 0, sizeof(*addr));         // Pulizia
+
+    // Configurazione dell'indirizzo
+    (*addr).sin_family = AF_INET;
+    (*addr).sin_port = htons(port);
+    (*addr).sin_addr.s_addr = INADDR_ANY;
+
+    // Aggancio dell'indirizzo
+    ret = bind(sd, (struct sockaddr*)addr, sizeof(*addr));
+
+    if(ret < 0){
+        perror("Errore in fase di bind: \n");
+        exit(-1); 
+    }
+
+    return sd;
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -17,13 +61,112 @@ int main(int argc, char *argv[])
 
     int port;                                       // Porta a cui e' associato il Server
     int ret;                                        // Variabile di controllo
+    fd_set master;                                  // Set principale gestito dal programmatore con le macro 
+    fd_set read_fds;                                // Set di lettura gestito dalla select 
+    int fdmax;                                      // Numero max di descrittori
+    struct sockaddr_in my_addr;                     // Indirizzo Server
+    //struct sockaddr_in cl_addr;                   // Indirizzo Client 
+    int listener;                                   // Socket per l'ascolto
+    //int newfd;                                    // Socket di comunicazione
+    //char buffer[1024];
+    int i;
 
-    // Si inizializza la porta del Server
+    // Questa funzione si occupa della prima stampa a video
+    //first_print();
+
+
+    // Si inizializza la porta del Server, atoi converte da char ad intero
     port = (argc > 1) ? atoi(argv[1]) : 4242;
+
+    // Azzero i set
+    FD_ZERO(&master);
+    FD_ZERO(&read_fds); 
+
+    // Questa funzione si occupa della configurazione del socket di ascolto:
+    // Il primo argomento riguarda un puntatore alla struttura dell'indirizzo del Server (struct sockaddr_in*)
+    // Il secondo argomento riguarda la porta a cui si deve connettere (int)
+    // La funzione restituisce il descrittore del socket di ascolto, chiamato listener
+    listener = listen_config(&my_addr, port);
+
+    // Il server crea una coda di 10 ascolti 
+    ret = listen(listener, 10);
+
+    if(ret < 0){
+        perror("Errore nella listen: ");
+        exit(-1);
+    }
+
+    // Aggiungo il listener al set master
+    FD_SET(listener, &master);
+    FD_SET(0, &master);
+
+    // Il socket maggiore sara' il listener
+    fdmax = listener;
+
+    // Ciclo principale
+
+    while(1){
+
+        read_fds = master;
+
+        printf("Parte il ciclo e chiamo la select\n");
+        select(fdmax + 1, &read_fds, NULL, NULL, NULL);
+
+         for(i=0; i<=fdmax; i++) {
+            // Cerco quelli pronti
+            if(FD_ISSET(i, &read_fds)) {                    // Trovato socket pronto
+
+                if(!i){                                     // Quello pronto riguarda la stdin
+                    printf("Vediamo se ho capito\n");
+                    FD_CLR(i, &master);
+                }
+
+                else if(i == listener) {                         // Se quello pronto e' il listener
+                    /*
+                    printf("Il listener (socket %d) e' pronto\n", i);
+                    sleep(5);
+                    addrlen = sizeof(cl_addr);
+                    // Accettiamo connessione su un nuovo socket fd
+                    newfd = accept(listener, (struct sockaddr *)&cl_addr, &addrlen);
+
+                    printf("Ho accettato la connessione, aggiungo nuovo socket al SET\n");
+
+                    //sleep(2);
+                    FD_SET(newfd, &master);                 // Aggiungo il nuovo socket al master
+                    fdmax=(newfd > fdmax) ? newfd : fdmax;  // Aggiorno fdmax
+                    */
+                } else{                                     // Se il socket pronto e' il comunicatore
+                    /*
+                    //sleep(5);
+                    ret = recv(i, buffer, sizeof(buffer), 0);
+                    if(!ret){                               // Socket i e' stato chiuso 
+                        printf("Socket chiuso\n");
+                        FD_CLR(i, &master);                 // Lo tolgo dal master 
+                        close(i);                           // Lo chiudo
+                        //sleep(2);
+                        printf("Chiudo il socket %d e lo tolgo dal set\n", i);
+
+                    } else if(ret > 0){                     // Qui arriva il SEGNALE /XXX
+
+                        printf("Il comunicatore (socket %d) e' pronto\n", i);
+                        printf("E' arrivato il segnale %s\n", buffer);
+                        
+                        
+                    } else{
+                        perror("Errore nella reiceve: ");
+                    }*/
+                }
+            }
+        }
+
+
+    }
 
 
 
     
-    printf("La porta del Server e' %d\n", port);
+
+
+    
     return 0;
 }
