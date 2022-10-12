c7fa54ed10c2e911f680d6b75f2aae936f06786e
whydela <t.delato@studenti.unipi.it>
2022-10-12 10:28:58 +0200

    vediamo

--- Server.c
+++ Server.c
@@ -1,10 +1,29 @@
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
-int main(){
-    int sono;
 
-    int ti_prego;
-    int perfavore;
+
+
+int main(int argc, char *argv[])
+{
+    // Dichiarazioni Variabili
+
+    int port;                                       // Porta a cui e' associato il Server
+    int ret;                                        // Variabile di controllo
+
+    // Si inizializza la porta del Server
+    port = (argc > 1) ? atoi(argv[1]) : 4242;
+
+
+
     
-
-
+    printf("La porta del Server e' %d\n", port);
+    return 0;
 }
